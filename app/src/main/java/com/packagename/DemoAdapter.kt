dd986fa0b5b88cbd461f22744cff19bf4ec40349
Abhishek.Tiwari <@sportzinteractive.net>
2023-10-05 13:55:01 +0530

    Added delete button where we can delete locally

--- app/src/main/java/com/packagename/DemoAdapter.kt
+++ app/src/main/java/com/packagename/DemoAdapter.kt
@@ -15,49 +15,58 @@ import com.packagename.mynotesapp.models.ArticleDto
 import com.packagename.mynotesapp.models.CreateArticleResponse
 import com.packagename.mynotesapp.models.ProgrammingItem
 
-class DemoAdapter(private val onArticleClicked:(ArticleDto) -> Unit):
+class DemoAdapter(private val onArticleClicked: (ArticleDto) -> Unit) :
     ListAdapter<ArticleDto, DemoAdapter.ViewHolder>(Diffutil()) {
 
-   inner class ViewHolder(private val binding : ItemArticelBinding) : RecyclerView.ViewHolder(binding.root){
+    private val listData: MutableList<ArticleDto> = mutableListOf()
 
-        fun bind(item: ArticleDto){
+    inner class ViewHolder(private val binding: ItemArticelBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ArticleDto, index: Int) {
             Log.d("author", item.author.toString())
             binding.txtUserName.text = item.author.username
             binding.date.text = item.createdAt
             binding.textViewArticleDescription.text = item.description
             binding.textViewArticleTitle.text = item.title
             binding.txtUserCircle.text = item.author.username[0].toString()
+
             binding.root.setOnClickListener {
                 onArticleClicked(item)
             }
+            Log.d("listdata size", listData.size.toString())
 
+            binding.delete.setOnClickListener {
+                if (index >= 0 && index < listData.size) {
+                    listData.removeAt(index)
+                    Log.d("list pos", index.toString())
+                    submitList(listData)
+                    notifyDataSetChanged()
+                }
+            }
         }
     }
 
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemArticelBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_articel,parent,false)
+        val binding = ItemArticelBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = getItem(position)
-        holder.bind(item)
+        holder.bind(item, position)
+        if (position < listData.size) {
+            listData[position] = item
+        } else {
+            listData.add(item)
+        }
     }
 
-    class Diffutil : DiffUtil.ItemCallback<ArticleDto>(){
+    class Diffutil : DiffUtil.ItemCallback<ArticleDto>() {
         override fun areItemsTheSame(oldItem: ArticleDto, newItem: ArticleDto): Boolean {
-           return oldItem.slug == newItem.slug
+            return oldItem.slug == newItem.slug
         }
 
-        override fun areContentsTheSame(
-            oldItem: ArticleDto,
-            newItem: ArticleDto
-        ): Boolean {
+        override fun areContentsTheSame(oldItem: ArticleDto, newItem: ArticleDto): Boolean {
             return oldItem == newItem
         }
-
     }
 }
